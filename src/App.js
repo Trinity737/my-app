ad41ed02a16a117e1d42ae9a027dc8c886473df9
Trinity737 <85462888+Trinity737@users.noreply.github.com>
2021-10-26 17:35:03 -0400

    updated bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Cheavon Pottinger and is{" "}
-        <a href="https://github.com/Trinity737/my-app" target="_blank">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was coded by Cheavon Pottinger and is{" "}
+          <a href="https://github.com/Trinity737/my-app" target="_blank">
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
