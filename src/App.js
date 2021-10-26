95a96da74f1c765d46d9fe8ff174937bbaf62553
Trinity737 <85462888+Trinity737@users.noreply.github.com>
2021-10-26 18:02:18 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
@@ -5,7 +6,6 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
-
         <footer>
           This project was coded by Cheavon Pottinger and is{" "}
           <a href="https://github.com/Trinity737/my-app" target="_blank">
